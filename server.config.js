1514ea965e9ded0507fd50507e2f60164f3420ee
navduhan <duhan27dec@gmail.com>
2026-02-22 01:32:40 -0600

    Fix /hupoxnet serving and add Babel dev dependency

--- server.config.js
+++ server.config.js
@@ -5,7 +5,7 @@ module.exports = {
       cwd: __dirname,
       script: 'npx',
       interpreter: 'none',
-      args: 'serve -s build -l 3860',
+      args: 'serve -s build -l 3860 -c serve.json',
       env: {
         NODE_ENV: 'production',
       },
